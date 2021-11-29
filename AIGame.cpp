daf7cd6c6a94025b977c9936e06cb0476cca537a
Dylann <dylannbatisse@gmail.com>
2021-11-29 18:26:52 +0100

    Changes by antoi

--- AIGame.cpp
+++ AIGame.cpp
@@ -3,12 +3,43 @@
 
 using namespace std;
 
+int maxFromArray(const int* tabValues) {
+    int max = -100;
+    int idx = 0;
+    for (int i = 0; i < SIZE; i++) {
+        if (tabValues[i] > max) {
+            max = tabValues[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+int minFromArray(const int* tabValues) {
+    int min = 100;
+    int idx = 0;
+    for (int i = 0; i < SIZE; i++) {
+        if (tabValues[i] < min) {
+            min = tabValues[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+void printTab(int* tab) {
+    for (int i = 0; i < SIZE; i++) {
+        printf(" [%d] ", tab[i]);
+    }
+    printf("\n");
+}
+
 
 int inputPlayer(bool isJ1) {
     if (isJ1) {
-        printf("\nJ1 : ");
+        printf("\n\nJ1 : ");
     } else {
-        printf("\nJ2 : ");
+        printf("\n\nJ2 : ");
     }
     int x;
     cin >> x;
@@ -22,9 +53,15 @@ void loop(Board board) {
         board.printCases();
         int x;
         bool validMove;
-        x = inputPlayer(board.getIsJ1());
+        if (board.getIsJ1()) {
+            x = minMax(board, board.getIsJ1(), 0, 7);
+            printf("\n\nJ1 IA minMax : %d\n\n", x);
+        }
+        else {
+            x = inputPlayer(board.getIsJ1());
+            printf("\n\nJ2 IA minMax : %d\n\n", x);
+        }
         validMove = board.play(x);
-
         if (validMove) {
             board.setNextPlayer();
         }
--- Board.cpp
+++ Board.cpp
@@ -1,5 +1,15 @@
 #include "Board.h"
 
+bool isEmpty(const int* currentCase) {
+    for (int i = 0; i < SIZE; i++) {
+        if (currentCase[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 int Board::minMax(Board currentBoard, int depth, int depthMax) {
     return 0;
 }
@@ -12,6 +22,20 @@ Board::Board() {
     isJ1 = true;
     J2Pieces = 0;
     J1Pieces = 0;
+
+}
+
+Board Board::copy() {
+    Board b = Board();
+    for (int i = 0; i < SIZE; i++) {
+        b.caseJ1[i] = caseJ1[i];
+        b.caseJ2[i] = caseJ2[i];
+    }
+    b.isJ1 = isJ1;
+    b.J2Pieces = J2Pieces;
+    b.J1Pieces = J1Pieces;
+
+    return b;
 }
 
 Board::~Board() {
@@ -27,12 +51,12 @@ int *Board::getCaseJ1() {
 }
 
 void Board::printCases() {
-    printf("\n Nouveau tour: \n");
+    printf("\n\n Nouveau tour: \n");
 
-    printf("pieces J1 : %d, pieces J2 : %d\n", J1Pieces, J2Pieces);
+    printf("pieces J1 : %d, pieces J2 : %d\n\n", J1Pieces, J2Pieces);
     printf("J2 ");
     for (int i = SIZE - 1; i >= 0; i--) {
-        printf("[%d] ", caseJ2[i]);
+        printf("|%d|-", caseJ2[i]);
     }
     printf("\nNp ");
     for (int i = SIZE - 1; i >= 0; i--) {
@@ -41,7 +65,7 @@ void Board::printCases() {
     printf("\n\n");
     printf("J1 ");
     for (int i = 0; i < SIZE; i++) {
-        printf("[%d] ", caseJ1[i]);
+        printf("|%d|-", caseJ1[i]);
     }
     printf("\nNp ");
     for (int i = 0; i < SIZE; i++) {
@@ -52,11 +76,6 @@ void Board::printCases() {
 
 bool Board::getIsJ1() {
     return isJ1;
-}
-
-
-void Board::setIsJ1(bool isPlaying) {
-    isJ1 = isPlaying;
 }
 
 
@@ -76,24 +95,30 @@ bool Board::play(int move) {
     }
 
     if (!checkValidMove(currentCase, move)) {
-        printf("Coup invalide\n");
+        //printf("Coup invalide\n");
         return false;
     }
 
     int index = move;
     currentCase[move] = 0;
+    int index = move;
 
     while (seeds > 0) {
         if (index >= SIZE - 1) {
             index = 0;
             currentCase = switchPlayer(&isCaseJ1);
-            currentCase[index]++;
         } else {
             index++;
-            currentCase[index]++;
         }
-        seeds--;
+
+        if (index == move && ((isCaseJ1 == getIsJ1()) || (!isCaseJ1 && !getIsJ1()))) {
+            continue;
+        } else {
+            currentCase[index]++;
+            seeds--;
+        }
     }
+
     eatSeeds(index, currentCase, isCaseJ1);
     return true;
 }
@@ -113,18 +138,21 @@ int *Board::switchPlayer(bool *isCaseJ1) {
 
 bool Board::isEnd() {
     if (isJ1) {
-        return J1Pieces >= 25;
+        return J1Pieces >= 25 || isEmpty(caseJ2);
     }
-    return J2Pieces >= 25;
+    return J2Pieces >= 25 || isEmpty(caseJ1);
 }
+
 
 void Board::setNextPlayer() {
     isJ1 = !isJ1;
 }
 
+
 bool Board::checkValidMove(const int *currentCase, int move) {
     return currentCase[move] > 0 && move >= 0 && move <= 5;
 }
+
 
 void Board::eatSeeds(int index, int *currentCase, bool isCaseJ1) {
     if (getIsJ1() == isCaseJ1) {
@@ -140,6 +168,7 @@ void Board::eatSeeds(int index, int *currentCase, bool isCaseJ1) {
         index--;
     }
 }
+
 
 void Board::addPieces(int pieces) {
     if (isJ1) {
