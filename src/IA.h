3dd070659ce69a114a83f87456e41168ab75b4a8
Dylann <dylannbatisse@gmail.com>
2021-12-12 20:36:15 +0100

    override

--- src/IA.h
+++ src/IA.h
@@ -14,9 +14,9 @@ using namespace std;
 
 int maxFromArray(const int *tabValues);
 int minFromArray(const int *tabValues);
-int negamax(Board &currentBoard, bool AIPlaying, int depth, int depthMax, long long *acc, bool isJ1, bool firstCall, int alpha, int beta, clock_t clock);
-int negamaxStart(Board &currentBoard, bool AIPlaying, int depth, int depthMax, long long *acc, bool isJ1, clock_t clock);
-int evaluateDepth(Board board, bool isJ1, int depthMax, int nbTour);
+int minmax_alphaBeta(Board &currentBoard, bool AIPlaying, int depth, int depthMax, long long *acc, bool isJ1, int alpha, int beta);
+int minmax_alphaBetaStart(Board &currentBoard, bool AIPlaying, int depth, int depthMax, long long *acc, bool isJ1);
+int evaluateDepth(Board board, bool isJ1, int depthMax);
 
 
 #endif //AI_GAME_IA_H
